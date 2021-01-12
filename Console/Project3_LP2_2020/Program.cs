357822aa99eea67effc63ad9b63d9147bed0fa54
FilipeVillar <filipevillar7@gmail.com>
2021-01-12 19:39:35 +0000

    Add project structure

---
+++ Console/Project3_LP2_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project3_LP2_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
